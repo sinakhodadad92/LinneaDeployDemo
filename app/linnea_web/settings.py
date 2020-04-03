dbd1a9f3bda19da4dea09faf990e21d4b44982de
Aravind <aravindsankaran22@gmail.com>
2020-04-03 21:50:22 +0000

    Linked with postgres

--- app/linnea_web/settings.py
+++ app/linnea_web/settings.py
@@ -98,12 +98,46 @@ WSGI_APPLICATION = 'linnea_web.wsgi.application'
 #    }
 #}
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'postgres',
+#        'USER': 'postgres',
+#        'HOST': 'db',
+#        'PORT': 5432,
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': 5432,
     }
 }
+
+#print(DATABASES)
+#exit(-1)
+
+#DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql',
+#            'HOST': 'db',
+#            'NAME': 'linnea_web',
+#            'USER': 'uaname',
+#            'PASS': 'password',
+#        }
+#}   
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
